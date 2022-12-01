b8a6ebc9e05258f28d5cf8c4a2ef45e1c4b304c9
TheAakashSingh <aakashskilldevelopment@gmail.com>
2022-12-01 16:44:13 +0530

    Updates

---
+++ precache-manifest.5df1ace35e3306f03a115836838052d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9785b5a77ff719512a9",
+    "url": "/expense-tracker/static/css/main.9a243ec6.chunk.css"
+  },
+  {
+    "revision": "b9785b5a77ff719512a9",
+    "url": "/expense-tracker/static/js/main.b9785b5a.chunk.js"
+  },
+  {
+    "revision": "f00868c99281e314bf54",
+    "url": "/expense-tracker/static/js/1.f00868c9.chunk.js"
+  },
+  {
+    "revision": "2c594b5b6bfedf8ec159",
+    "url": "/expense-tracker/static/js/runtime~main.2c594b5b.js"
+  },
+  {
+    "revision": "86a476b86493edd4c3e8b7cc38de3fa3",
+    "url": "/expense-tracker/index.html"
+  }
+];
